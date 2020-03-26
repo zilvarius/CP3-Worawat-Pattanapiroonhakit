4a40d2cfb246cc3d29082803c8499f500a99d90a
zilvarius <62667501+zilvarius@users.noreply.github.com>
2020-03-26 23:50:36 +0700

    Add Exercise5_2

---
+++ Assignment/Exercise5_2_Worawat_P.py
@@ -0,0 +1,7 @@
+print("------------------------------")
+print("       Speed Calculator       ")
+
+s = int(input("Distance (Km) : "))
+t = int(input("Time (Hr) : "))
+v = s/t
+print("Speed : ",v , "Km/Hr")
