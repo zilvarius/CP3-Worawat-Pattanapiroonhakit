6ccd25490caeaa0fba5d75efb32af1cf1491ffbc
zilvarius <zilva_free@msn.com>
2020-03-26 19:31:39 +0700

    Complete Project

--- Assignment/Exercise4_Worawat_P.py
+++ Assignment/Exercise4_Worawat_P.py
@@ -1,4 +1,12 @@
-en = 
-
+eng = 88
+bis = 85
+comsys = 92
+compro = 90
+p = "Point"
 print("--- Your Score ---")
+print("Foundation English               :",eng,p)
+print("General Business                 :",bis,p)
+print("Introduction To Computer Systems :",comsys,p)
+print("Computer Programming             :",compro,p)
+
 
