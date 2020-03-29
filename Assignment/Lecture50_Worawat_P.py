a5f61829ba446ec02b123de2794672bbcde3aafa
zilvarius <zilva_free@msn.com>
2020-03-30 01:32:57 +0700

    Add Lecture50 Homework

---
+++ Assignment/Lecture50_Worawat_P.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x+y)
+def devideNumber(x,y):
+    print(float(x/y))
+
+addNumber(15,3)
+minusNumber(15,3)
+multiplyNumber(15,3)
+devideNumber(15,3)
